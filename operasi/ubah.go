6d84d6bae4febbe415c3f2f2f003e77710fb4323
Rymaxw <155986913+Rymaxw@users.noreply.github.com>
2025-05-22 11:28:45 +0700

    Update ubah.go

--- operasi/ubah.go
+++ operasi/ubah.go
@@ -6,20 +6,26 @@ import (
     "time"
 )
 
-// UbahBahanMakanan mengubah data bahan makanan berdasarkan indeks
-func UbahBahanMakanan(indeks int, nama string, jumlahStok int, tanggalKedaluwarsa string) {
-    if indeks < 0 || indeks >= models.JumlahData {
-        fmt.Println("Indeks tidak valid.")
+
+func UbahBahanMakanan(ID int, nama string, jumlahStok int, tanggalKedaluwarsa string) {
+
+    if ID < 1 || ID > models.JumlahData {
+        fmt.Println("ID tidak valid.")
         return
     }
 
+   
+    indeksArray := ID - 1
+
+  
     tgl, err := time.Parse("02-01-2006", tanggalKedaluwarsa)
     if err != nil {
         fmt.Println("Format tanggal salah. Gunakan format DD-MM-YYYY.")
         return
     }
 
-    models.StokBahanMakanan[indeks] = models.BahanMakanan{
+
+    models.StokBahanMakanan[indeksArray] = models.BahanMakanan{
         Nama:           nama,
         JumlahStok:     jumlahStok,
         TanggalKedaluwarsa: tgl,
